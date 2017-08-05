fa14370d6b5ad0497820f46ada04017fe003a57b
AlenMiletic <alen.miletic6@gmail.com>
2017-08-05 15:24:56 +0200

    Completed

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,6 +14,9 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+	raise TriangleError if (a <= 0 || b<=0 || c<=0)
+	x,y,z = [a,b,c].sort
+	raise TriangleError if x + y <= z
 	if(a == b && a == c) 
 		return :equilateral
 	elsif(a == b || a == c || b == c)
